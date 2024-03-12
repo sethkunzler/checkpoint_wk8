f77e6be4cddcb49fc86043713dfe959800d1c83d
Seth Kunzler <sethkunzler@gmail.com>
2024-03-12 11:49:46 -0600

    notebook layout

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,5 +1,23 @@
 <template>
-
+  <!-- TODO fun landing page -->
+  <h1 class="text-center mt-2">Welcome to Noter!</h1>
+  <div class="container">
+    <section class="row">
+      <div class="col-md-12">
+        <h2>My Notebooks</h2>
+      </div>
+    </section>
+    <section class="row my-2">
+      <div class="col-md-6 d-flex">
+        <div class="tab rounded bg-dark shadow border border-subtle p-1"></div>
+        <div class="rounded border border-subtle px-3 pt-1 shadow selectable">
+          <h3>A Notebook</h3>
+          <img class="img-fluid notebook-img" src="../assets/img/noter-logo.png" alt="filler">
+          <p class="text-end">Entries: 12</p>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
@@ -31,5 +49,10 @@ export default {
       object-position: center;
     }
   }
+  .tab {
+
+  }
 }
+
+notebook-img {}
 </style>
