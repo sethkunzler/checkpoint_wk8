75d15cf65307b464b97533cf0f383285f9dd90ea
Seth Kunzler <sethkunzler@gmail.com>
2024-03-13 16:32:36 -0600

    Passing postman tests: (restrict requests: 1,2, 4,5) (todo: restrict requests to edit another user's notebook)

--- server/src/controllers/EntriesController.js
+++ server/src/controllers/EntriesController.js
@@ -9,6 +9,8 @@ export class EntriesController extends BaseController {
       .use(Auth0Provider.getAuthorizedUserInfo)// middleware
       .post('', this.createEntry)
       .get('', this.getMyEntries)
+      .put('/:entryId', this.editEntry)
+      .delete('/:entryId', this.deleteEntry)
   }
   async createEntry(request, response, next) {
     try {
@@ -25,6 +27,27 @@ export class EntriesController extends BaseController {
       const accountId = request.userInfo.id 
       const entries = await entriesService.getMyEntries(accountId)
       response.send(entries)
+    } catch (error) {
+      next(error)
+    }
+  }
+  async editEntry(request, response, next) {
+    try {
+      const entryId = request.params.entryId
+      const userId = request.userInfo.id
+      const newData = request.body
+      const message = await entriesService.editEntry(entryId, userId, newData)
+      response.send(message)
+    } catch (error) {
+      next(error)
+    }
+  }
+  async deleteEntry(request, response, next) {
+    try {
+      const entryId = request.params.entryId 
+      const userId = request.userInfo.id
+      await entriesService.deleteEntry(entryId, userId)
+      response.send("Entry was successfully deleted")
     } catch (error) {
       next(error)
     }
--- server/src/services/EntriesService.js
+++ server/src/services/EntriesService.js
@@ -1,8 +1,14 @@
 import { dbContext } from "../db/DbContext.js"
+import { BadRequest, Forbidden } from "../utils/Errors.js"
+import { notebooksService } from "./NotebooksService.js"
 
 class EntriesService {
-
   async createEntry(data) {
+    // FIXME the validation for the notebook's creator ID matching the user ID does not work
+    // const notebook = await notebooksService.getNotebookById(data.notebookId)
+    // if (notebook.creatorId != data.creatorId) {
+    //   throw new Forbidden("YOU ARE NOT THE CREATOR OF THIS NOTEBOOK! ACCESS TO EDIT IS RESTRICTED TO THE CREATOR ONLY!")
+    // }
     const entry = await dbContext.Entries.create(data)
     await entry.populate('notebook')
     return entry
@@ -14,6 +20,50 @@ class EntriesService {
   async getNotebookEntries(notebookId) {
     const entries = await dbContext.Entries.find({notebookId}).populate('notebook')
     return entries
+  }
+  async getEntryById(entryId) {
+    const entry = await dbContext.Entries.findById(entryId).populate('notebook')
+    if (!entry) {
+      throw new BadRequest(`Entry Not Found! No _id matches the given Entry ID: ${entryId}`)
+    }
+    return entry
+  }
+  async editEntry(entryId, userId, newData) { 
+    const entry = await this.getEntryById(entryId)
+    if (entry.creatorId != userId) {
+      throw new Forbidden("YOU ARE NOT THE CREATOR OF THIS ENTRY! ACCESS TO EDIT IS RESTRICTED TO THE CREATOR ONLY!")
+    }
+    // FIXME  the validation for matching the notebook's creator ID to the user ID does not work 
+    // needs to check if there is a notebook and then check if the creator Id matches the user ID
+    // if (newData.notebook && newData.notebook.creatorId != userId) {
+    //   throw new Forbidden("YOU ARE NOT THE CREATOR OF THIS NOTEBOOK! ACCESS TO EDIT IS RESTRICTED TO THE CREATOR ONLY!")
+    // }
+
+    // description string
+    entry.description = newData.description == undefined ? 
+    entry.description 
+    : 
+    newData.description
+    // image url string
+    entry.img = newData.img == undefined ? 
+    entry.img
+    :
+    newData.img 
+    // Notebook ID 
+    entry.notebookId = newData.notebookId == undefined ? 
+    entry.notebookId
+    :
+    newData.notebookId
+    // saving the updates before returning it out - Updates will not save if they are not filled out correctly
+    await entry.save()
+    return entry
+  }
+  async deleteEntry(entryId, userId) {
+    const entry = await this.getEntryById(entryId)
+    if (entry.creatorId != userId) {
+      throw new Forbidden("YOU ARE NOT THE CREATOR OF THIS ENTRY! ACCESS TO DELETE IS RESTRICTED TO THE CREATOR ONLY!")
+    }
+    await entry.deleteOne()
   }
 }
 
